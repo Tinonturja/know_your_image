46ebe9c8fd08ee1e41d7e1010af82c12bc771013
Tinonturja <tinonturja@gmail.com>
2026-02-15 05:41:59 +0600

    Install the necessary library & get the web page content

---
+++ choose_url.py
@@ -0,0 +1,14 @@
+# importing libraries
+from bs4 import BeautifulSoup
+import requests
+
+def get_the_page(url:str):
+    # headers
+    headers = {"User-Agent": "Mozila/5.0"}
+    # get the url
+    url = url
+    # get the page
+    responses = requests.get(url, headers=headers)
+    return BeautifulSoup(responses.text, "html.parser")
+content = get_the_page("https://en.wikipedia.org/wiki/Dassault_Rafale")
+print(content)
