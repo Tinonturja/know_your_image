0463ada8431865c60da0922482c4770ff9ed813a
Tinonturja <tinonturja@gmail.com>
2026-02-15 08:19:32 +0600

    Get img list from webpage

--- choose_url.py
+++ choose_url.py
@@ -1,19 +1,24 @@
 # importing libraries
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import urljoin
 
 def get_the_page(url:str):
     # img_url list
     img_url_list = []
     # headers
     headers = {"User-Agent": "Mozila/5.0"}
-    # get the url
-    url = url
     # get the page
     responses = requests.get(url, headers=headers)
+    responses.raise_for_status()
+
     content = BeautifulSoup(responses.text, "html.parser")
     images = content.find_all('img')
     for img in images:
-        img_url_list.append(f"https:/{img['src']}")
+        src = img.get('src')
+        if src and src.endswith(('jpg', 'jpeg', 'png', 'svg')):
+            full_url = urljoin(url,src)
+            img_url_list.append(full_url)
+        
     return img_url_list
 
