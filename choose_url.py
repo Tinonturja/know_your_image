1c4a7489f0b6d1ef7245187ae2023a5ced9b33b5
Tinonturja <tinonturja@gmail.com>
2026-02-15 07:17:09 +0600

    Get url of the image from the webpage

--- choose_url.py
+++ choose_url.py
@@ -3,11 +3,17 @@ from bs4 import BeautifulSoup
 import requests
 
 def get_the_page(url:str):
+    # img_url list
+    img_url_list = []
     # headers
     headers = {"User-Agent": "Mozila/5.0"}
     # get the url
     url = url
     # get the page
     responses = requests.get(url, headers=headers)
-    return BeautifulSoup(responses.text, "html.parser")
+    content = BeautifulSoup(responses.text, "html.parser")
+    images = content.find_all('img')
+    for img in images:
+        img_url_list.append(f"https:/{img['src']}")
+    return img_url_list
 
